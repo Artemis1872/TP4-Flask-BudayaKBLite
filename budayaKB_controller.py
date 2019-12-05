b4c17d6363027d20c21238cfa9a491000b962028
Artemis1872 <dennisaw2000@gmail.com>
2019-12-05 23:42:18 +0700

    Statistik completed

--- budayaKB_controller.py
+++ budayaKB_controller.py
@@ -29,7 +29,7 @@ ALLOWED_EXTENSIONS = {'csv', 'txt'}
 # merender tampilan default(index.html)
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 # Bagian ini adalah implementasi fitur Impor Budaya, yaitu:
@@ -38,106 +38,120 @@ def index():
 # - menampilkan notifikasi bahwa data telah berhasil diimport 	
 @app.route('/impor', methods=['GET', 'POST'])
 def importData():
-	if request.method == "GET":
-		return render_template("impor.html")
+    if request.method == "GET":
+        return render_template("impor.html")
 
-	elif request.method == "POST" and 'imporcsv' in request.files:
+    elif request.method == "POST" and 'imporcsv' in request.files:
 
-		f = request.files['imporcsv']
-		savepath = os.path.join(UPLOAD_FOLDER, f.filename)
+        f = request.files['imporcsv']
+        savepath = os.path.join(UPLOAD_FOLDER, f.filename)
 
-		f.save(savepath)
-		databasefilename = f.filename
-		count = budayaData.importFromCSV(savepath)
-		os.remove(savepath)
-		#budayaData.exportToCSV("exported.csv")  # setiap perubahan data langsung disimpan ke file
-		if count > 0:
-			return render_template("impor.html", count=count, filename=databasefilename, impor=1)
-		else:
-			return render_template("impor.html", impor=0)
+        f.save(savepath)
+        databasefilename = f.filename
+        count = budayaData.importFromCSV(savepath)
+        os.remove(savepath)
+        # budayaData.exportToCSV("exported.csv")  # setiap perubahan data langsung disimpan ke file
+        if count > 0:
+            return render_template("impor.html", count=count, filename=databasefilename, impor=1)
+        else:
+            return render_template("impor.html", impor=0)
 
-	elif request.method == "POST":
-		flag = budayaData.exportToCSV("exported.csv")
+    elif request.method == "POST":
+        flag = budayaData.exportToCSV("exported.csv")
 
-		if flag == 1:
-			with open("exported.csv") as fp:
-				csv = fp.read()
-				return Response(
-					csv,
-					mimetype="text/csv",
-					headers={"Content-disposition": "attachment; filename=BudayaKB-Lite.csv"})
-		else:
-			return render_template("impor.html", ekspor=0)
+        if flag == 1:
+            with open("exported.csv") as fp:
+                csv = fp.read()
+                return Response(
+                    csv,
+                    mimetype="text/csv",
+                    headers={"Content-disposition": "attachment; filename=BudayaKB-Lite.csv"})
+        else:
+            return render_template("impor.html", ekspor=0)
 
 
 @app.route('/tambah', methods=['GET', 'POST'])
 def tambahData():
-	if request.method == 'GET':
-		return render_template("tambah.html", data=budayaData.koleksi)
+    if request.method == 'GET':
+        return render_template("tambah.html", data=budayaData.koleksi)
 
-	elif request.method == "POST":
-		datainput = request.form  # Mengambil data dan membuat kedalam dictionary
-		success = budayaData.tambah(datainput['Nama'], datainput['Tipe'], datainput['Provinsi'], datainput['Referensi'])
-		return render_template("tambah.html", result=datainput['Nama'], data=budayaData.koleksi, success=success)
+    elif request.method == "POST":
+        datainput = request.form  # Mengambil data dan membuat kedalam dictionary
+        success = budayaData.tambah(datainput['Nama'], datainput['Tipe'], datainput['Provinsi'], datainput['Referensi'])
+        return render_template("tambah.html", result=datainput['Nama'], data=budayaData.koleksi, success=success)
 
 
 @app.route('/ubah', methods=['GET', 'POST'])
 def ubahData():
-	if request.method == 'GET':
-		return render_template("ubah.html", data=budayaData.koleksi)
+    if request.method == 'GET':
+        return render_template("ubah.html", data=budayaData.koleksi)
 
-	elif request.method == 'POST':
-		datainput = request.form
-		success = budayaData.ubah(datainput['Nama'], datainput['Tipe'], datainput['Provinsi'], datainput['Referensi'])
-		return render_template("ubah.html", terubah=datainput['Nama'], result=success, data=budayaData.koleksi)
+    elif request.method == 'POST':
+        datainput = request.form
+        success = budayaData.ubah(datainput['Nama'], datainput['Tipe'], datainput['Provinsi'], datainput['Referensi'])
+        return render_template("ubah.html", terubah=datainput['Nama'], result=success, data=budayaData.koleksi)
 
 
 @app.route('/hapus', methods=['GET', 'POST'])
 def hapusData():
-	if request.method == 'GET':
-		return render_template("hapus.html", data=budayaData.koleksi)
+    if request.method == 'GET':
+        return render_template("hapus.html", data=budayaData.koleksi)
 
-	elif request.method == 'POST':
-		datainput = request.form
-		deletion = budayaData.hapus(datainput['Nama'])
-		return render_template("hapus.html", result=deletion, terhapus=datainput['Nama'], data=budayaData.koleksi)
+    elif request.method == 'POST':
+        datainput = request.form
+        deletion = budayaData.hapus(datainput['Nama'])
+        return render_template("hapus.html", result=deletion, terhapus=datainput['Nama'], data=budayaData.koleksi)
 
 
 @app.route('/cari', methods=['GET', 'POST'])
 def cariData():
-	if request.method == 'GET':
-		return render_template("cari.html", kumpulbudaya=budayaData.koleksi)
+    if request.method == 'GET':
+        return render_template("cari.html", kumpulbudaya=budayaData.koleksi)
 
-	elif request.method == 'POST':
-		tipe = request.form['Tipe']
-		teks = request.form['Teks']
-		print(tipe)
-		print(teks)
+    elif request.method == 'POST':
+        tipe = request.form['Tipe']
+        teks = request.form['Teks']
+        print(tipe)
+        print(teks)
 
-		if tipe == 'Nama':
-			result = budayaData.cariByNama(teks)
-			return render_template("cari.html", result=result)
-		elif tipe == 'Tipe':
-			result = budayaData.cariByTipe(teks)
-			return render_template("cari.html", result=result)
+        if tipe == 'Nama':
+            result = budayaData.cariByNama(teks)
+            return render_template("cari.html", result=result)
+        elif tipe == 'Tipe':
+            result = budayaData.cariByTipe(teks)
+            return render_template("cari.html", result=result)
 
-		elif tipe == 'Provinsi':
-			result = budayaData.cariByProv(teks)
-			return render_template("cari.html", result=result)
-		else:
-			return render_template("cari.html", kumpulbudaya=budayaData.koleksi)
+        elif tipe == 'Provinsi':
+            result = budayaData.cariByProv(teks)
+            return render_template("cari.html", result=result)
+        else:
+            return render_template("cari.html", kumpulbudaya=budayaData.koleksi)
 
 
 @app.route('/statistik', methods=['GET', 'POST'])
 def statData():
-	if request.method == 'GET':
-		banyak = budayaData.stat()
-		return render_template("stat.html")
+    # Stat Prov
+    provsorteddata = sorted(budayaData.statByProv().items(), key=lambda x: x[1], reverse=True)
+    provlistofkey = [x[0] for x in provsorteddata]
+    provlistofvalue = [x[1] for x in provsorteddata]
+    provnums = len(provlistofkey)
 
-	elif request.method == 'POST':
-		return render_template("stat.html")
+    # Stat Tipe
+    tipesorteddata = sorted(budayaData.statByTipe().items(), key=lambda x: x[1], reverse=True)
+    tipelistofkey = [x[0] for x in tipesorteddata]
+    tipelistofvalue = [x[1] for x in tipesorteddata]
+    tipenums = len(tipelistofkey)
+
+    # Stat
+    stat = budayaData.stat()
+    if request.method == 'GET':
+        return render_template("stat.html", databudaya=budayaData.koleksi, provlabel=provlistofkey,
+                               provdata=provlistofvalue, provnums=provnums,
+                               provraw=provsorteddata, tipelabel=tipelistofkey,
+                               tipedata=tipelistofvalue, tipenums=tipenums,
+                               tiperaw=tipesorteddata, stat=stat)
 
 
 # run main app
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
